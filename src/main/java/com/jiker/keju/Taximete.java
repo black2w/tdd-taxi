fe70cc971039fd88e08f52e433e14c46a89cb7cb
wangwei <electronicwang@126.com>
2020-05-03 22:33:05 +0800

    覆盖率

--- src/main/java/com/jiker/keju/Taximete.java
+++ src/main/java/com/jiker/keju/Taximete.java
@@ -5,7 +5,9 @@ package com.jiker.keju;
 public class Taximete {
 
     public int queryPriceByMileAndTime(int mile, int time) {
-        if (mile <= 2) {
+        if (mile <=0) {
+            return 0;
+        } else if (mile <= 2) {
             return (int) Math.round(6 + time * 0.25);
         } else if (mile > 2 && mile <= 8) {
             return (int) Math.round((6 + (mile - 2) * 0.8) + time * 0.25);
